30a80cd2e6760276f3f31cfad9f83b1cf3b3f381
Mateusz Gliwka <gliwson@gmail.com>
2020-02-06 08:40:10 +0100

    Initial commit

---
+++ src/main/java/io/github/e1s/e1s/E1sApplication.java
@@ -0,0 +1,13 @@
+package io.github.e1s.e1s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E1sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/e1s/e1s/E1sApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.e1s.e1s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
