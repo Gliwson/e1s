58618e7ed2058d67cc27093b20311868d7b2d563
Mateusz Gliwka <gliwson@gmail.com>
2020-02-08 01:12:14 +0100

    fixed

--- src/main/java/io/github/e1s/e1s/repository/DiscountRepository.java
+++ src/main/java/io/github/e1s/e1s/repository/DiscountRepository.java
@@ -3,7 +3,6 @@ package io.github.e1s.e1s.repository;
 import io.github.e1s.e1s.domain.Discount;
 import io.github.e1s.e1s.domain.enums.TypeMaleFemaleKid;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
 
-    Optional<Discount> findByType(@Param("type") TypeMaleFemaleKid string);
+    //    @Query("SELECT Discount from Discount discount where discount.type =:type")
+    Optional<Discount> findByType(TypeMaleFemaleKid typeMaleFemaleKid);
 
 }
